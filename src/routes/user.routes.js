afdebea60ea098e0347cb416b9ad0ba1b5783d09
Delta Student <udaydhanokar35@gmail.com>
2024-07-22 23:27:50 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import  {registerUser, loginUser, logoutUser,refreshAccessToken } from '../controllers/user.controllers.js'
+import  {registerUser, loginUser, logoutUser,refreshAccessToken,changeCurrentPassword, getCurrentUser, updateAccoutDetails ,updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from '../controllers/user.controllers.js'
 import { upload } from '../middlewares/mulder.middlewares.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 const router = Router()
@@ -25,4 +25,11 @@ router.route("/register").post(
 //secured routes
     router.route ("/logout").post(verifyJWT, logoutUser)
     router.route ("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+    router.route("/curret-user").get(verifyJWT,getCurrentUser),
+    router.route("/update-account").patch(verifyJWT,updateAccoutDetails)//ethe galti na pn post noko thu nahi ta sagde update haoil
+    router.route("/avatar").patch(verifyJWT,upload.single('avatar'),updateUserAvatar)
+    router.route("/coverImage").patch(verifyJWT,upload.single('coverImage'),updateUserCoverImage)
+    router.route('/c/:username').get(verifyJWT,getUserChannelProfile)
+    router.route("/history").get(verifyJWT,getWatchHistory)
 export default router
